48c4cbb22be4e8741fa48e65773db236c2b02d19
joningi98 <joningi17201@gmail.com>
2019-08-14 17:56:50 +0000

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args){
+        System.out.println("This will be printed");
+
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+    };
+}
