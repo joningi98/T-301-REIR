36acf95a8ed455d3ac144271a735d510976799ee
joningi98 <joningi17201@gmail.com>
2019-09-03 17:54:43 +0000

    S1

--- src/S1/Percolation.java
+++ src/S1/Percolation.java
@@ -1,48 +1,49 @@
+package S1;
+import edu.princeton.cs.algs4.QuickUnionUF;
+
 public class Percolation {
 
     private Boolean[][] grid;
+    private int top = 0;
+    private int bot = 0;
+    private QuickUnionUF UF;
 
     public Percolation(int N){  // create N-by-N grid, with all sites initially blocked
+        UF = new QuickUnionUF(N*N+2);
+        top = N*N + 2;
+        bot = N*N + 2;
         grid = new Boolean[N][N];
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 grid[i][j] = false;
             }
         }
-
     }
     public void open(int row, int col){ // open the site (row, col) if it is not open already
         grid[row][col] = true;
-
-        if (isOpen(row, col + 1)){ // left
-            
-        }
-
-        if (isOpen(row, col - 1)){ // Right
-
-        }
-        if (isOpen(row + 1, col)){ // Up
-
-        }
-
-        if (isOpen(row - 1, col + 1)){ // Down
-
-        }
-
-
     }
     public boolean isOpen(int row, int col){ // is the site (row, col) open?
         return grid[row][col];
     }
     public boolean isFull(int row, int col){ // is the site (row, col) full?
+        return UF.connected()
 
     }
     public int numberOfOpenSites(){ // number of open sites
-
+        int count = 0;
+        for(int row = 0; row < grid.length - 1; row++){
+            for(int col = 0; col < grid.length - 1; col++){
+                if (grid[row][col]){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
     public boolean percolates(){  // does the system percolate?
-
+        return false;
     }
     public static void main(String[] args){ // unit testing (required)
+
     }
 }
