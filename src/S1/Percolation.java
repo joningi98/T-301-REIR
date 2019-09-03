e71f2e0763e3a106d2beaff08e506c7f43a9b45e
joningi98 <joningi17201@gmail.com>
2019-09-03 20:16:33 +0000

    s1

--- src/S1/Percolation.java
+++ src/S1/Percolation.java
@@ -7,11 +7,13 @@ public class Percolation {
     private int top = 0;
     private int bot = 0;
     private QuickUnionUF UF;
+    private int gridSize = 0;
 
     public Percolation(int N){  // create N-by-N grid, with all sites initially blocked
-        UF = new QuickUnionUF(N*N+2);
+        UF = new QuickUnionUF(N*N+3);
         top = N*N + 2;
         bot = N*N + 2;
+        gridSize = N;
         grid = new Boolean[N][N];
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
@@ -20,20 +22,38 @@ public class Percolation {
         }
     }
     public void open(int row, int col){ // open the site (row, col) if it is not open already
-        grid[row][col] = true;
+        grid[col][row] = true;
+        if(row + 1 < gridSize && isOpen(row + 1, col)){ // Right
+           UF.union(getGridPoint(row + 1, col), getGridPoint(row, col));
+        }
+        if(row - 1 >= 0 && isOpen(row - 1, col)){ // Left
+            UF.union(getGridPoint(row - 1, col), getGridPoint(row, col));
+        }
+        if(col - 1 >= 0 && isOpen(row, col - 1)){ // Up
+            UF.union(getGridPoint(row, col - 1), getGridPoint(row, col));
+        }
+        if(col + 1 < gridSize && isOpen(row, col + 1)){ // Down
+            UF.union(getGridPoint(row, col + 1), getGridPoint(row, col));
+        }
     }
     public boolean isOpen(int row, int col){ // is the site (row, col) open?
-        return grid[row][col];
+        return grid[col][row];
     }
     public boolean isFull(int row, int col){ // is the site (row, col) full?
-        return UF.connected()
-
+        if (isOpen(col, row)){
+            for(int i = 0; i < gridSize; i++){
+                if(UF.connected(getGridPoint(row, col), i)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     public int numberOfOpenSites(){ // number of open sites
         int count = 0;
-        for(int row = 0; row < grid.length - 1; row++){
-            for(int col = 0; col < grid.length - 1; col++){
-                if (grid[row][col]){
+        for(int row = 0; row < grid.length; row++){
+            for(int col = 0; col < grid.length; col++){
+                if (grid[col][row]){
                     count++;
                 }
             }
@@ -45,5 +65,9 @@ public class Percolation {
     }
     public static void main(String[] args){ // unit testing (required)
 
+    }
+
+    private int getGridPoint(int row, int col){
+        return (col*10) + row;
     }
 }
